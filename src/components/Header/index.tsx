9c7410b8bf354fdb8cd8d610cd6af89c509c10d6
lfogawa <lfogawa0@gmail.com>
2023-08-19 22:53:52 -0300

    medicine list page updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,20 @@
+import logo from "../../assets/svg/logo.svg"
+
 function Header() {
   return (
     <>
+      <header>
+        <img src={logo} alt="logo" width={50}/>
+        <nav>
+          <ul>
+            <li><a href="/map">Map</a></li>
+            <li><a href="/medicinelist">Medicine List</a></li>
+            <li><a href="/medicineregistration">Medicine Registration</a></li>
+            <li><a href="/pharmacyregistration">Pharmacy Registration</a></li>
+            <li><a href="/">Logout</a></li>
+          </ul>
+        </nav>
+      </header>
     </>
   )
 }
--- src/pages/MedicineList/index.tsx
+++ src/pages/MedicineList/index.tsx
@@ -1,9 +1,29 @@
-
+import { useEffect, useState } from "react"
+import { SearchField } from '../../components/SearchField'
+import { Card } from '../../components/Card'  // Certifique-se de importar o componente Card corretamente
 
 function MedicineList() {
+  const existingMedicine = JSON.parse(localStorage.getItem('itemMedicineData') || '[]');
+  const [medicineList, setMedicineList] = useState(existingMedicine);
+
+  useEffect(() => {
+    setMedicineList(existingMedicine);
+  }, []);
+
   return (
     <>
-      
+      <SearchField />
+      {medicineList.map((medicine: any, index: number) => (
+        <Card
+          key={index}
+          medicineName={medicine.medicineName}
+          labName={medicine.labName}
+          dosage={medicine.dosage}
+          description={medicine.description}
+          unitPrice={medicine.unitPrice}
+          type={medicine.type}
+        />
+      ))}
     </>
   )
 }
